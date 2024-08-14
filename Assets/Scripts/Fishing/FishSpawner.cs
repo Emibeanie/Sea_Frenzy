7303b2f435bd4b908fbe77d070f61b13b83f4cd0
Emibeanie <emily.gendelev@gmail.com>
2024-08-14 18:39:58 +0300

    added fish spawn limit

--- Assets/Scripts/Fishing/FishSpawner.cs
+++ Assets/Scripts/Fishing/FishSpawner.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
@@ -6,6 +8,9 @@ public class FishSpawner : MonoBehaviour
     public RectTransform panelRectTransform;
     public float spawnInterval = 2.0f;
 
+    private float _fishCount;
+    private float _fishSpawnLimit = 24f;
+
     void Start()
     {
         InvokeRepeating("SpawnFish", 0, spawnInterval);
@@ -13,20 +18,24 @@ public class FishSpawner : MonoBehaviour
 
     void SpawnFish()
     {
-        GameObject fish = fishPool.GetFish();
-        fish.transform.SetParent(panelRectTransform, false);
+        while (_fishCount < _fishSpawnLimit)
+        {
+            GameObject fish = fishPool.GetFish();
+            fish.transform.SetParent(panelRectTransform, false);
 
-        RectTransform fishRectTransform = fish.GetComponent<RectTransform>();
-        Debug.Log(panelRectTransform.rect.width);
-        Debug.Log(panelRectTransform.rect.height);
+            RectTransform fishRectTransform = fish.GetComponent<RectTransform>();
+            Debug.Log(panelRectTransform.rect.width);
+            Debug.Log(panelRectTransform.rect.height);
 
-        float width = panelRectTransform.rect.width - fishRectTransform.rect.width;
-        float height = panelRectTransform.rect.height - fishRectTransform.rect.height;
-        float x = Random.Range(-width / 2, width / 2);
-        float y = Random.Range(-height / 2, height / 2);
-        fishRectTransform.anchoredPosition = new Vector2(x, y);
+            float width = panelRectTransform.rect.width - fishRectTransform.rect.width;
+            float height = panelRectTransform.rect.height - fishRectTransform.rect.height;
+            float x = Random.Range(-width / 2, width / 2);
+            float y = Random.Range(-height / 2, height / 2);
+            fishRectTransform.anchoredPosition = new Vector2(x, y);
 
-        Debug.Log("Fish spawned at: " + fishRectTransform.anchoredPosition);
+            Debug.Log("Fish spawned at: " + fishRectTransform.anchoredPosition);
+            _fishCount++;
+        }
     }
 }
 
