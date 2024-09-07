2718dfe0d2a33e0c6ec93ea92db8d7cdd482b539
Emibeanie <emily.gendelev@gmail.com>
2024-09-07 16:44:35 +0300

    animations update & JSON playerPrefs

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using Photon.Pun;
 
+
 public class PlayerMovment : MonoBehaviourPun
 {
     [SerializeField] float moveSpeed = 2f;
     [SerializeField] Animator animator;
     /*[SerializeField] float runSpeed = 5f;*/
+    private PlayerData playerData = new PlayerData();
 
     private void Start()
     {
@@ -13,26 +15,61 @@ public class PlayerMovment : MonoBehaviourPun
         {
             animator = GetComponent<Animator>();
         }
+
+        if (photonView.IsMine)
+        {
+            LoadPlayerData();
+        }
     }
 
     void MovePlayer()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        if (photonView.IsMine)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 movment = new Vector2(horizontal, vertical).normalized * moveSpeed * Time.deltaTime;
-        transform.position += movment;
+            Vector3 movment = new Vector2(horizontal, vertical).normalized * moveSpeed * Time.deltaTime;
+            transform.position += movment;
+
+            //update player data for saving
+            playerData.positionX = transform.position.x;
+            playerData.positionY = transform.position.y;
+            playerData.positionZ = transform.position.z;
+        }
     }
 
     private void Update()
     {
-       MovePlayer();
-       UpdateAnimation();
+        if (photonView.IsMine)
+        {
+            MovePlayer();
+            UpdateAnimation();
+        }
     }
     void UpdateAnimation()
     {
         bool isWalking = Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0;
 
         animator.SetBool("isWalking", isWalking);
+    }
+
+    public void SavePlayerData()
+    {
+        string jsonData = JsonUtility.ToJson(playerData);
+        PlayerPrefs.SetString("PlayerData", jsonData);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadPlayerData()
+    {
+        if (PlayerPrefs.HasKey("PlayerData"))
+        {
+            string jsonData = PlayerPrefs.GetString("PlayerData");
+            playerData = JsonUtility.FromJson<PlayerData>(jsonData);
+
+            //restore player position and state
+            transform.position = new Vector3(playerData.positionX, playerData.positionY, playerData.positionZ);
+        }
     }
 }
